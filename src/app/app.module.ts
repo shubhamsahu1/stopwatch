db33a3bd296de3ffc5c97dad118b69fa9d9aa9c7
shubhamsahu16 <shubhamsahu.sahu@gmail.com>
2020-04-18 15:23:18 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StopwatchComponent } from './stopwatch/stopwatch.component';
+import { TimerComponent } from './timer/timer.component';
+import { ControlComponent } from './control/control.component';
+import { DisplayComponent } from './display/display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StopwatchComponent,
+    TimerComponent,
+    ControlComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/stopwatch/stopwatch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'stopwatch',
+  templateUrl: './stopwatch.component.html',
+  styleUrls: ['./stopwatch.component.scss']
+})
+export class StopwatchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
