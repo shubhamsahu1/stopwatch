6e20ad5a857c6ee9cae849e65917ff8c11abf0f3
shubhamsahu16 <shubhamsahu.sahu@gmail.com>
2020-04-18 16:28:37 +0530

    updating timer component

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'timer',
   templateUrl: './timer.component.html',
-  styleUrls: ['./timer.component.css']
+  styleUrls: ['./timer.component.scss']
 })
 export class TimerComponent implements OnInit {
 
@@ -11,5 +11,5 @@ export class TimerComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  
 }
