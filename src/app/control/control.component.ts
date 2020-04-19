0fcf314b7cbb396223c93f7c3b19d09625168c59
shubhamsahu16 <shubhamsahu.sahu@gmail.com>
2020-04-20 00:53:29 +0530

    counter start

--- src/app/control/control.component.ts
+++ src/app/control/control.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'control',
   templateUrl: './control.component.html',
   styleUrls: ['./control.component.scss']
 })
-export class ControlComponent implements OnInit {
+export class ControlComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
-  ngOnInit(): void {
+  intervalId = 0;
+  message = '';
+  seconds = 11;
+
+  startCount: number = 0
+  stopCount: number = 0
+
+  setTime(time: number) {
+    this.seconds = time
+  }
+
+  clearTimer() { clearInterval(this.intervalId); }
+
+  ngOnInit() { }
+
+  ngOnDestroy() { this.clearTimer(); }
+  start() { this.countDown(); }
+  stop() {
+    this.clearTimer();
+    this.message = `Holding at T-${this.seconds} seconds`;
+  }
+  private countDown() {
+    this.clearTimer();
+    this.intervalId = window.setInterval(() => {
+      this.seconds -= 1;
+      if (this.seconds === 0) {
+        this.message = 'Blast off!';
+      } else {
+        if (this.seconds < 0) { this.seconds = 10; } // reset
+        this.message = `T-${this.seconds} seconds and counting`;
+      }
+    }, 1000);
   }
 
 }
--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'timer',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timer.component.scss']
 })
 export class TimerComponent implements OnInit {
-
+  @Input() seconds:number
   constructor() { }
 
   ngOnInit(): void {
